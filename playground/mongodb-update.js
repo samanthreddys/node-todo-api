55edb63f623303de178974204cef17b196da1006
Samanth reddy Chinthakuntla <>
2018-01-03 19:45:39 -0600

    hello

---
+++ playground/mongodb-update.js
@@ -0,0 +1,59 @@
+const { MongoClient, ObjectID } = require('mongodb');
+
+
+
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (error, db) => {
+    if (error) {
+        console.log('Unable to connect to MongoDB Server');
+    }
+    console.log('Connected to MongoDB Succesfully');
+
+    //delete many
+    /*     db.collection('users').deleteMany({ name: 'hello' }).then((result) => {
+            console.log('users');
+            console.log(JSON.stringify(result, undefined, 2));
+
+        }, (err) => {
+            console.log('Unable to fetch users', err);
+
+
+        }); */
+
+
+    // delete one
+    /* 
+        db.collection('users').deleteOne({ name: 'hello' }).then((result) => {
+            console.log('users');
+            console.log(JSON.stringify(result, undefined, 2));
+
+        }, (err) => {
+            console.log('Unable to fetch users', err);
+
+
+        }); */
+
+
+
+    // findOne and delete
+    db.collection('users').findOneAndUpdate({ name: 'hello' }, {
+            $set: { name: "Hi" }
+        },
+        false).then((result) => {
+        console.log('users');
+        console.log(JSON.stringify(result, undefined, 2));
+
+    }, (err) => {
+        console.log('Unable to fetch users', err);
+
+
+    });
+
+
+
+
+
+
+
+    db.close();
+});
