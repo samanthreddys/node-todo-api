b8fca0ed406b1b7555f66d422f598180ff006bc4
Samanth reddy Chinthakuntla <>
2018-01-03 20:36:55 -0600

    hello

--- playground/mongodb-update.js
+++ playground/mongodb-update.js
@@ -36,10 +36,24 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (error, db) => {
 
 
     // findOne and delete
-    db.collection('users').findOneAndUpdate({ name: 'hello' }, {
-            $set: { name: "Hi" }
-        },
-        false).then((result) => {
+    /*     db.collection('users').findOneAndUpdate({ name: 'Hi' }, {
+                $set: { name: "Helo" },
+                $inc: { age: -26 }
+            },
+            false).then((result) => {
+            console.log('users');
+            console.log(JSON.stringify(result, undefined, 2));
+
+        }, (err) => {
+            console.log('Unable to fetch users', err);
+
+
+        });
+     */
+    db.collection('users').update({ name: 'Hello' }, {
+        $set: { name: "Hi" },
+        $inc: { age: -26 }
+    }, { upsert: true, multi: true }).then((result) => {
         console.log('users');
         console.log(JSON.stringify(result, undefined, 2));
 
